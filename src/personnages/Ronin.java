3993b445e3b2b3d516672d9603972984d24e54ab
gireaud noemie <grn4213a@ens.fsi>
2022-10-27 15:24:22 +0200

    debut Ronin

---
+++ src/personnages/Ronin.java
@@ -0,0 +1,11 @@
+package personnages;
+
+public class Ronin extends Humain {
+	private int honneur = 1;
+		
+	public Ronin(String nom, String boisson, int argent) {
+		super(nom, boisson, argent);
+	}
+
+	
+}
